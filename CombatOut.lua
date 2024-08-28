b66bb6c33ad41d73b45939c9a955e04a6008ff3f
bugRanger <departmentofjusticeevil@gmail.com>
2024-08-28 19:37:41 +0700

    Add command handler

--- CombatOut.lua
+++ CombatOut.lua
@@ -1,3 +1,23 @@
-local print = function(msg) if msg then DEFAULT_CHAT_FRAME:AddMessage(msg) end end
+local Name = "COMBATOUT"
+local ShortName = "CO"
+local SlashCommandFull = "/combatout"
+local SlashCommandShort = "/co"
 
-print("Hello World!")
+local Parameters = {}
+Parameters.debugMode = true
+
+
+local debug = function (msg)
+	if not Parameters.debugMode then return end
+	DEFAULT_CHAT_FRAME:AddMessage(string.format("%s debug '%s'", ShortName, msg))
+end
+
+function OnChatCommand(msg)
+	msg = msg or ""
+	debug(string.format("handle command - '%s'", msg))
+end
+
+SLASH_COMBATOUT1 = SlashCommandFull
+SLASH_COMBATOUT2 = SlashCommandShort
+
+SlashCmdList[Name] = OnChatCommand;
