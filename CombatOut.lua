72271622c7d7c3d2df6a8d66781d5d8b32c5bf46
bugRanger <departmentofjusticeevil@gmail.com>
2024-08-28 20:44:37 +0700

    Add handle combat events

--- CombatOut.lua
+++ CombatOut.lua
@@ -1,4 +1,4 @@
-local Name = "COMBATOUT"
+local Name = "CombatOut"
 local ShortName = "CO"
 local SlashCommandFull = "/combatout"
 local SlashCommandShort = "/co"
@@ -6,16 +6,34 @@ local SlashCommandShort = "/co"
 local Parameters = {}
 Parameters.debugMode = true
 
-
 local debug = function (msg)
 	if not Parameters.debugMode then return end
 	DEFAULT_CHAT_FRAME:AddMessage(string.format("%s debug '%s'", ShortName, msg))
 end
 
+
 function OnChatCommand(msg)
 	msg = msg or ""
 	debug(string.format("handle command - '%s'", msg))
 end
+
+debug("create frame")
+local frame = CreateFrame("Frame")
+
+function OnCombatIn() 
+	debug("handle event - combat in")
+	frame:SetScript("OnEvent", OnCombatOut)
+	frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+end
+
+function OnCombatOut()
+	debug("handle event - combat out")
+	frame:SetScript("OnEvent", OnCombatIn)
+	frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+end
+
+OnCombatOut()
+
 
 SLASH_COMBATOUT1 = SlashCommandFull
 SLASH_COMBATOUT2 = SlashCommandShort
